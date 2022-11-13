dc683e1cf3f0b5d4a94ca6eb4c65ab6f805d9376
cristtian177 <cristtian177@gmail.com>
2022-11-13 15:45:44 -0500

    update alert

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -36,7 +36,7 @@ export class LoginComponent implements OnInit {
       Swal.fire({
         position: 'center',
         icon: 'success',
-        title: 'Your work has been saved',
+        title: '',
         showConfirmButton: false,
         timer: 1500,
         
